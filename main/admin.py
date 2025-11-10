790c3a74b81cda844988d4569b23200006646c37
Bohdan Barulin <x0ryz@proton.me>
2025-11-10 08:04:24 +0000

    add models with admin registration

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Category, Model, ProductImage
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'category', 'model')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "categories"
+
+class Model(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    model = models.ForeignKey(Model, related_name='products', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d')
+    alt_text = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.image.name}'
