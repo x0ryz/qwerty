2552665ddcc2d8e939c82c5bf451632b3419a79f
Bohdan <x0ryz@proton.me>
2025-11-18 19:32:02 +0200

    add celery and rabbitmq

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,7 @@ from cart.cart import Cart
 
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -23,6 +24,7 @@ def order_create(request):
                 )
 
             cart.clear()
+            order_created.delay(order.id)
             return render(request, "orders/order/created.html", {"order": order})
     else:
         form = OrderCreateForm()
