ebfce4cf2bf4c2d22051014d5823c9d46b6c14e8
Bohdan <x0ryz@proton.me>
2025-11-26 18:05:23 +0200

    add devcontainer

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "shop"
+app_name = "main"
 
 
 urlpatterns = [
--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+app_name = 'shop'
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("cart/", include("cart.urls", namespace="cart")),
