26af8bca7d5210b9183933384aad00133315d70c
Bohdan Barulin <x0ryz@proton.me>
2025-12-06 14:14:23 +0200

    feat(cart):  update detail template for improved cart display

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processors.cart',
+                'cart.context.cart',
             ],
         },
     },
