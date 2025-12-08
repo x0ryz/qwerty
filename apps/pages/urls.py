4f7e4d3c8407b8613542aedfa7538997482f8eae
Bohdan Barulin <x0ryz@proton.me>
2025-12-08 18:42:52 +0200

    feat(pages): add index view and template for product display

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from . import views
 from .views import AboutView
 
 app_name = "pages"
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("about/", AboutView.as_view(), name="about"),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,12 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.db.models import Count
+from catalog.models import Product
+
+def index(request):
+    products = Product.objects.annotate(orders=Count('order_items')).order_by('-orders')[:4]
+
+    return render(request, 'pages/index.html', {'products': products})
 
 class AboutView(TemplateView):
     template_name = "pages/about.html"
