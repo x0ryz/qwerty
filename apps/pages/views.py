4c1b6944df34d5959467820f8ba72198022a010a
Bohdan Barulin <x0ryz@proton.me>
2025-12-08 20:29:23 +0200

    feat(pages): update product display logic to show minimum price and main image

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from django.db.models import Count
-from catalog.models import Product
+from django.db.models import Count, Min, Prefetch
+from catalog.models import Product, ProductImage
+
 
 def index(request):
-    products = Product.objects.annotate(orders=Count('order_items')).order_by('-orders')[:4]
-
+    main_images = ProductImage.objects.filter(is_main=True)
+    products = Product.objects.annotate(orders=Count('order_items'), min_price=Min('variants__price')).prefetch_related(Prefetch('images', queryset=main_images, to_attr='main_img_list')).order_by('-orders')[:4]
     return render(request, 'pages/index.html', {'products': products})
 
 class AboutView(TemplateView):
