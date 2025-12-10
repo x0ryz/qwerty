c097774f12beb6d38af376c00b9a23f9ac031c6d
Bohdan <x0ryz@proton.me>
2025-12-10 23:06:12 +0200

    feat(pages): refactor index view to use class-based view and update URL routing

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import AboutView
+from .views import AboutView, IndexView
 
 app_name = "pages"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
     path("about/", AboutView.as_view(), name="about"),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -4,14 +4,21 @@ from django.db.models import Count, Min, Prefetch
 from catalog.models import Product, ProductImage
 
 
-def index(request):
-    products = Product.objects.annotate(
-        orders=Count('variants__order_items'),
-        min_price=Min('variants__price')
-    ).prefetch_related(
-        Prefetch('images', queryset=ProductImage.objects.filter(is_main=True), to_attr='main_img_list')
-    ).order_by('-orders')[:4]
-    return render(request, 'pages/index.html', {'products': products})
+class IndexView(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['popular_products'] = Product.objects.annotate(
+            orders=Count('variants__order_items'),
+            min_price=Min('variants__price')
+        ).prefetch_related(
+            Prefetch('images', queryset=ProductImage.objects.filter(
+                is_main=True), to_attr='main_img_list')
+        ).order_by('-orders')[:4]
+
+        return context
 
 class AboutView(TemplateView):
     template_name = "pages/about.html"
