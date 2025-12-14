e0feae8e7c58fb36289cf2f4220375c1b01cccc3
Bohdan Barulin <x0ryz@proton.me>
2025-12-14 17:31:10 +0200

    refactor(catalog): add jsonb to product

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -9,8 +9,11 @@ class Attribute(models.Model):
     def __str__(self):
         return self.name
 
+
 class AttributeValue(models.Model):
-    attribute = models.ForeignKey(Attribute, related_name='values', on_delete=models.CASCADE)
+    attribute = models.ForeignKey(
+        Attribute, related_name="values", on_delete=models.CASCADE
+    )
     value = models.CharField(max_length=50)
 
     def __str__(self):
@@ -32,6 +35,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Brand(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
@@ -39,12 +43,16 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
     brand = models.ForeignKey(Brand, related_name="products", on_delete=models.CASCADE)
     description = models.TextField(blank=True)
+    specs = models.JSONField(default=dict, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -52,7 +60,7 @@ class Product(models.Model):
         return reverse("catalog:product_detail", args=[self.slug])
 
     def get_main_image_url(self):
-        if hasattr(self, 'main_img_list'):
+        if hasattr(self, "main_img_list"):
             if self.main_img_list:
                 return self.main_img_list[0].image.url
 
@@ -76,18 +84,25 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class ProductVariant(models.Model):
-    product = models.ForeignKey(Product, related_name="variants", on_delete=models.CASCADE)
-    attributes = models.ManyToManyField(AttributeValue, related_name='variants', blank=True)
+    product = models.ForeignKey(
+        Product, related_name="variants", on_delete=models.CASCADE
+    )
+    attributes = models.ManyToManyField(
+        AttributeValue, related_name="variants", blank=True
+    )
 
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sku = models.CharField(max_length=255, unique=True)
     available = models.BooleanField(default=True)
 
     def get_image_url(self):
-        attr_ids = self.attributes.values_list('id', flat=True)
+        attr_ids = self.attributes.values_list("id", flat=True)
 
-        specific_image = self.product.images.filter(attribute_value__id__in=attr_ids).first()
+        specific_image = self.product.images.filter(
+            attribute_value__id__in=attr_ids
+        ).first()
 
         if specific_image:
             return specific_image.image.url
@@ -97,35 +112,20 @@ class ProductVariant(models.Model):
     def __str__(self):
         return f"{self.product.name} (SKU: {self.sku})"
 
+
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, related_name="images", on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name="images", on_delete=models.CASCADE
+    )
     attribute_value = models.ForeignKey(
         AttributeValue,
         related_name="images",
         on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        blank=True,
     )
     image = models.ImageField(upload_to="products/%Y/%m/%d/", blank=True)
     is_main = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Img for {self.product.name}"
-
-
-class KeyboardSpecification(models.Model):
-    product = models.OneToOneField(Product, related_name="keyboard_specs", on_delete=models.CASCADE)
-    layout = models.CharField(max_length=50, blank=True)
-    connection = models.CharField(max_length=50, blank=True)
-    hot_swappable = models.BooleanField(default=False)
-
-class KeycapSpecification(models.Model):
-    product = models.OneToOneField(Product, related_name="keycap_specs", on_delete=models.CASCADE)
-    material = models.CharField(max_length=50, blank=True)
-    profile = models.CharField(max_length=50, blank=True)
-
-class SwitchSpecification(models.Model):
-    product = models.OneToOneField(Product, related_name="switch_specs", on_delete=models.CASCADE)
-    type = models.CharField(max_length=50, blank=True)
-    actuation_force = models.CharField(max_length=20, blank=True)
-    pin_count = models.IntegerField(default=3)
