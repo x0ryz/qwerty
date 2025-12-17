4918a5a3d43fcfab06ec0728ee4991c3cc750b12
Bohdan Barulin <x0ryz@proton.me>
2025-12-17 23:25:55 +0200

    feat(pages): add new_arrivals

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -12,15 +12,17 @@ class IndexView(TemplateView):
 
         images_qs = ProductImage.objects.order_by("sort_order")
 
-        context["popular_products"] = (
-            Product.objects.annotate(
-                orders=Count("variants__order_items"), min_price=Min("variants__price")
-            )
-            .prefetch_related(
-                Prefetch("images", queryset=images_qs, to_attr="main_img_list")
-            )
-            .order_by("-orders")[:4]
+        base_qs = Product.objects.annotate(
+            min_price=Min("variants__price")
+        ).prefetch_related(
+            Prefetch("images", queryset=images_qs, to_attr="main_img_list")
         )
+
+        context["popular_products"] = base_qs.annotate(
+            orders=Count("variants__order_items")
+        ).order_by("-orders")[:4]
+
+        context["new_arrivals"] = base_qs.order_by("-created")[:4]
 
         return context
 
